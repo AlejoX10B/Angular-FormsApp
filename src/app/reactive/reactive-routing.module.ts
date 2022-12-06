59493ec8d0db094d654638e4cd94d1b29fbc4db7
Alejandro Castañeda <fcastaneda@cidenet.com.co>
2022-12-05 22:51:40 -0500

    Configuración de app, creación de módulos, rutas y componentes

---
+++ src/app/reactive/reactive-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BasicsComponent } from './basics/basics.component';
+import { DynamicsComponent } from './dynamics/dynamics.component';
+import { SwitchesComponent } from './switches/switches.component';
+
+const module = 'Reactive -';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'basics', title: `${module} Basics`, component: BasicsComponent },
+      { path: 'dynamics', title: `${module} Dynamics`, component: DynamicsComponent },
+      { path: 'switches', title: `${module} Switches`, component: SwitchesComponent },
+      { path: '**', redirectTo: 'basics' },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ReactiveRoutingModule { }
